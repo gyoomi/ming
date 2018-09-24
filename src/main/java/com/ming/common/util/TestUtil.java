b0cf6c3217efd68000536638875615a1b49d2d12
gyoomi <gyoomi0709@foxmail.com>
2018-09-24 23:11:41 +0800

    Initial commit

---
+++ src/main/java/com/ming/common/util/TestUtil.java
@@ -0,0 +1,16 @@
+/**
+ * Copyright © 2018, LeonKeh
+ * <p>
+ * All Rights Reserved.
+ */
+
+package com.ming.common.util;
+
+/**
+ * 类功能描述
+ *
+ * @author Leon
+ * @version 2018/9/24 21:08
+ */
+public class TestUtil {
+}
