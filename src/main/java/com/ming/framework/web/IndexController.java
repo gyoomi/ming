7b8ab6ee39f974387ab5b8084c62d73180736ad5
gyoomi <gyoomi0709@foxmail.com>
2018-09-25 11:25:50 +0800

    初步完成日志和thymeleaf集成

---
+++ src/main/java/com/ming/framework/web/IndexController.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright © 2018, TaoDing
+ * <p>
+ * All Rights Reserved.
+ */
+
+package com.ming.framework.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * 类功能描述
+ *
+ * @author Leon
+ * @version 2018/9/25 10:44
+ */
+@Controller
+public class IndexController {
+    private static Logger lg = LoggerFactory.getLogger(IndexController.class);
+
+    @GetMapping(value = "/index")
+    public ModelAndView index() {
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("user", "小明");
+        mv.setViewName("/index.html");
+        return mv;
+    }
+}
